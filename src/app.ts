996338341cd5c2f5b9226bf57f4f01c23cb5262f
ihenyenpaul@gmail.com <akhere.ihenyen@kudabank.com>
2022-02-16 19:21:33 +0100

    set up routing

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { corsUrl, environment } from './config';
 import './database'; // initialize database
+import routesV1 from './routes/v1';
 
 process.on('uncaughtException', (e) => {
   console.error(e);
@@ -13,5 +14,8 @@ const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
 app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
+
+// Routes
+app.use('/v1', routesV1);
 
 export default app;
