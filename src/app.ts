8a943786450126d29d6ec54e4d1b5694a615b037
ihenyenpaul@gmail.com <akhere.ihenyen@kudabank.com>
2022-02-16 19:24:42 +0100

    add response constants in middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { corsUrl, environment } from './config';
 import './database'; // initialize database
+import { NotFoundError, ApiError, InternalError } from './core/ApiError';
 import routesV1 from './routes/v1';
 
 process.on('uncaughtException', (e) => {
@@ -17,5 +18,22 @@ app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
 
 // Routes
 app.use('/v1', routesV1);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => next(new NotFoundError()));
+
+// Middleware Error Handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    ApiError.handle(err, res);
+  } else {
+    if (environment === 'development') {
+      console.error(err);
+      return res.status(500).send(err.message);
+    }
+    ApiError.handle(new InternalError(), res);
+  }
+});
 
 export default app;
